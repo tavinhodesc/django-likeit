e6877c926e07c4df584b874a3f1dca90bdf3af82
Agustín Méndez <matagus@gmail.com>
2013-12-09 20:08:12 -0300

    Rasing verion to 0.1 and releasing it.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open('README.md').readlines()
 
 setup(
     name='django-favit',
-    version='0.0.1',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
