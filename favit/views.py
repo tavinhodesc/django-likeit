bceb1211e8dfac4bdc92699a2d7d7739aee10890
Agustín Méndez <matagus@gmail.com>
2013-12-09 17:24:55 -0300

    Refactoring add_or_remove view to use manager querysets.

--- favit/views.py
+++ favit/views.py
@@ -1,36 +1,40 @@
-from django.http import HttpResponse
-from django.db.models import get_model
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseBadRequest
 from django.utils import simplejson
-from django.contrib.contenttypes.models import ContentType
 
 from .models import Favorite
 
 
+@login_required
 def add_or_remove(request):
-    if request.is_ajax():
-        user = request.user
-        target_model = get_model(*request.POST['target_model'].split('.') or None)
-        target_content_type = ContentType.objects.get_for_model(target_model)
-        target_object_id = request.POST['target_object_id']
 
-        # delete it if it's already a faorite
-        if user.favorite_set.filter(target_content_type=target_content_type,
-                                 target_object_id=target_object_id):
-            user.favorite_set.get(target_content_type=target_content_type,
-                                     target_object_id=target_object_id).delete()
-            status = 'deleted'
+    if not request.is_ajax():
+        return HttpResponseNotAllowed()
 
-        # otherwise, create it
-        else:
-            user.favorite_set.create(target_content_type=target_content_type,
-                                     target_object_id=target_object_id)
-            status = 'added'
+    user = request.user
 
-        response = {'status': status,
-                    'fav_count': Favorite.objects.filter(target_content_type=target_content_type,
-                                                         target_object_id=target_object_id).count()}
+    try:
+        app_model = request.POST["target_model"]
+        obj_id = request.POST["target_object_id"]
+    except KeyError:
+        return HttpResponseBadRequest()
 
-        return HttpResponse(simplejson.dumps(response, ensure_ascii=False),
-                            mimetype='application/json')
+    fav = Favorite.objects.get_favorite(user, obj_id, model=app_model)
 
-    return HttpResponse(status=405)
+    if fav is None:
+        Favorite.objects.create(user, obj_id, app_model)
+        status = 'added'
+    else:
+        fav.delete()
+        status = 'deleted'
+
+    response = {
+        'status': status,
+        'fav_count': Favorite.objects.for_object(obj_id, app_model).count()
+    }
+
+    return HttpResponse(
+        simplejson.dumps(response, ensure_ascii=False),
+        mimetype='application/json'
+    )
