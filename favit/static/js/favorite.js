83bfffe5280fb58d27525f18495e0c3ddd8fa6a4
Pablo Mouzo <pablomouzo@gmail.com>
2014-07-10 17:35:05 -0300

    Remove jQuery ajax CSRF headers setting
    This should not be done by a library, this is standard Django configuration that should be in any project that uses ajax posting.
    Also, this conflicts with custom CSRF settings, like different cookie name.

--- favit/static/js/favorite.js
+++ favit/static/js/favorite.js
@@ -1,26 +1,3 @@
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-var csrftoken = getCookie('csrftoken');
-$.ajaxSetup({
-    beforeSend: function(xhr) {
-	xhr.setRequestHeader("X-CSRFToken", csrftoken);
-    }
-});
-
 $('.btn.favorite').click(function() {
     var $obj = $(this);
     var target_id = $obj.attr('id').split('_')[1];
