793502f217d1541c5c06aab11997542c09374910
Gustavo Guella <gustavodesc@gmail.com>
2020-05-13 16:24:01 -0300

    Adjustments on button and pypi setup

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@
 
 from setuptools import setup, find_packages
 
-try:
-    import pypandoc
-    README = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    README = open('README.md').read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='django-likeit-ptbr',
-    version='0.1.2',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
-    description=README[2].rstrip('\n'),
-    long_description=''.join(README),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/gustavodesc/django-likeit-ptbr',
     download_url='https://github.com/gustavodesc/django-likeit-ptbr/tarball/0.1',
     author='Gustavo Guella',
